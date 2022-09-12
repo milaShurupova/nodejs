1495285fd1bf79b5b8974c6b21b20aa70c9d4708
milaShurupova <mila.shurupova@gmail.com>
2022-09-12 17:48:23 +0300

    NP-15: post (add) updated

--- source/constants.ts
+++ source/constants.ts
@@ -25,16 +25,17 @@ export class SqlParameters {
 }
 
 export class Queries {
-    public static WhiteBoardTypes: string = `SELECT * FROM white_board_type`;
-    public static WhiteBoardTypeByID: string = `SELECT * FROM white_board_type WHERE id = ?`;
-    public static UpdateWhiteBoardTypeByID: string = `UPDATE white_board_type SET white_board_type = ? WHERE id = ?`;
+    public static WhiteBoardTypes: string = `SELECT * FROM white_board_type WHERE status_id = ?`;
+    public static WhiteBoardTypeByID: string = `SELECT * FROM white_board_type WHERE id = ? AND status_id = ?`;
+    public static UpdateWhiteBoardTypeByID: string = `UPDATE white_board_type SET white_board_type = ?, update_date = ?, update_user_id = ? WHERE id = ? AND status_id = ?`;
     public static WhiteBoardTypeByTitle: string = `SELECT * FROM white_board_type WHERE id = ?`;
-    public static AddWhiteBoardType: string = `INSERT white_board_type (white_board_type) VALUES (?)`;
+    public static AddWhiteBoardType: string = `INSERT white_board_type (white_board_type, create_date, update_date, create_user_id, update_user_id, status_id) VALUES (?, ?, ?, ?, ?, ?)`;
     public static SelectIdentity: string = `SELECT SCOPE_IDENTITY() AS id`;
-    public static DeleteBoardTypeByID: string = `DELETE FROM white_board_type WHERE id = ?`;
+    public static DeleteBoardTypeByID: string = `UPDATE white_board_type SET update_date = ?, update_user_id = ?, status_id = ? WHERE id = ? AND status_id = ?`;
 
 }
 
 export const DB_CONNECTION_STRING: string = "server=.;Database=masa_school;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 export const NON_EXISTENT_ID: number = -1;
+export const TEMP_USER_ID: number = 1;
 
--- source/routes/school.routes.ts
+++ source/routes/school.routes.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/general/board-types', controller.getBoardTypes);
 router.get('/general/board-type/:id', controller.getBoardTypebyId);
 router.put('/general/board-type/:id', controller.updateBoardTypeById);
-// router.put('/general/board-type/:title', controller.getBoardTypebyTitle);
+router.get('/general/board-type-by-title/:title', controller.getBoardTypeByTitle);
 router.post('/general/board-type', controller.addBoardType);
 router.delete('/general/board-type/:id', controller.deleteBoardTypeById);
 
