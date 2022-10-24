11df2c546e2fae7ade5dabb82452bcea7e2286d0
milaShurupova <mila.shurupova@gmail.com>
2022-10-24 12:03:17 +0300

    Basic TS sever works

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+
+class UserController {
+
+  constructor() {}
+
+  async getUserByID(req: any, res: Response, next: NextFunction) {
+
+
+    
+    
+    // return res.status(200).json({
+    //   success: true,
+    //   data: [
+    //     {
+    //       name: "John",
+    //     },
+    //     {
+    //       name: "Steve",
+    //     },
+    //   ],
+    // });
+  }
+}
+
+export default new UserController();
